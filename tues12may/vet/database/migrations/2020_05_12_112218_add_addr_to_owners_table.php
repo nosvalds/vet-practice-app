701611caaf478c05cd094165a1fb1e557b82cec1
Nik Osvalds <nosvalds@gmail.com>
2020-05-12 12:30:43 +0100

    03 migrations complete

---
+++ tues12may/vet/database/migrations/2020_05_12_112218_add_addr_to_owners_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAddrToOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            $table->string("address_1",100);
+            $table->string("address_2",100);
+            $table->string("town",100);
+            $table->string("postcode",11);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            $table->dropColumn("address_1");
+            $table->dropColumn("address_2");
+            $table->dropColumn("town");
+            $table->dropColumn("postcode");
+        });
+    }
+}
