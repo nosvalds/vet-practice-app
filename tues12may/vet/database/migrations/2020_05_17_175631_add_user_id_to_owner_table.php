12791cbcf99480475b228e8fa013cb1c60214a4c
Nik Osvalds <nosvalds@gmail.com>
2020-05-17 19:14:04 +0100

    add user id to owners table

---
+++ tues12may/vet/database/migrations/2020_05_17_175631_add_user_id_to_owner_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToOwnerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            // add user_id foreign key to owners table
+            $table->foreignId('user_id')->unsigned();
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            // remove
+            $table->dropForeign('owners_user_id_foreign');
+            $table->dropColumn("user_id");
+        });
+    }
+}
