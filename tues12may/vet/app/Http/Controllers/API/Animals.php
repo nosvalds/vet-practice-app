eb774f6c25d026489258cb09d3d50ca793c6c4e6
Nik Osvalds <nosvalds@gmail.com>
2020-05-18 14:36:39 +0100

    Animals API

---
+++ tues12may/vet/app/Http/Controllers/API/Animals.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Animal;
+
+class Animals extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return all Animals from the database
+        return Animal::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // get request data
+        $data = $request->all();
+
+        // create new record in DB
+        return Animal::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Animal $animal)
+    {
+        // return the requested animal info
+        return $animal;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Animal $animal)
+    {
+        // get request data
+        $data = $request->all();
+
+        // update in the database
+        $animal->update($data);
+
+        // return updated version
+        return $animal;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Animal $animal)
+    {
+        // delete animal from the DB
+        $animal->delete();
+
+        // return 204
+        return response(null, 204);
+    }
+}
--- tues12may/vet/routes/api.php
+++ tues12may/vet/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Animals;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Animals
+Route::group(["prefix" => "/animals"], function () {
+    Route::get('/','API\Animals@index');
+    Route::get('/{animal}','API\Animals@show');
+    Route::delete('/{animal}','API\Animals@destroy');
+    Route::post('/','API\Animals@store');
+    Route::put('/{animal}','API\Animals@update');
+});
+
