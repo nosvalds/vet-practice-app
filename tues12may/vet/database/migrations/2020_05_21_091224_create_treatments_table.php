c2510c5f4b51f928b4701a524dbc094b2fee004e
Nik Osvalds <nosvalds@gmail.com>
2020-05-21 11:40:29 +0100

    making sure treatments migration was saved appropriately

--- tues12may/vet/database/migrations/2020_05_21_091224_create_treatments_table.php
+++ tues12may/vet/database/migrations/2020_05_21_091224_create_treatments_table.php
@@ -13,9 +13,22 @@ class CreateTreatmentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('treatments', function (Blueprint $table) {
+        Schema::create("treatments", function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("name",100);
+        });
+
+        Schema::create("animal_treatment", function (Blueprint $table) {
+            $table->id();
+
+            // create animal id column and foreign key
+            $table->foreignId("animal_id")->unsigned();
+            $table->foreign("animal_id")->references("id")->on("animals")->onDelete("cascade");
+
+            // create treatment id column and foreign key
+            $table->foreignId("treatment_id")->unsigned();
+            $table->foreign("treatment_id")->references("id")->on("treatments")->onDelete("cascade");
+
         });
     }
 
@@ -26,6 +39,7 @@ class CreateTreatmentsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('animal_treatment');
         Schema::dropIfExists('treatments');
     }
 }
