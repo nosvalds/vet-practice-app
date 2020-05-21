5b0ce1e15a1ec14382db64feb75271ce2e6e6f95
Nik Osvalds <nosvalds@gmail.com>
2020-05-21 13:05:05 +0100

    Animal API controller tests created

---
+++ tues12may/vet/tests/Unit/Http/Controllers/API/AnimalsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\API;
+
+use PHPUnit\Framework\TestCase;
+
+class AnimalsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
