1d532ac15c0b7a8a40332fab1ffa5b319775e491
Nik Osvalds <nosvalds@gmail.com>
2020-06-06 14:14:45 +0100

    update deploy to path

--- deployment/deploy/production.rb
+++ deployment/deploy/production.rb
@@ -7,4 +7,4 @@ set :branch, "master"
 
 # it should deploy to the following directory
 # update with the appropriate directory on your server
-set :deploy_to, "/var/www/vet-practice-app"
+set :deploy_to, "/var/www/vet-practice-app/"
