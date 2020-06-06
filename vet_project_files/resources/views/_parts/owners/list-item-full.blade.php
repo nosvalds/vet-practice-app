47e1903ec1653a3034d33cad00e177020ac890a9
Nik Osvalds <nosvalds@gmail.com>
2020-06-06 11:06:47 +0100

    remove previous copy of files

--- vet_project_files/resources/views/_parts/owners/list-item-full.blade.php
+++
@@ -1,9 +0,0 @@
-<a href="/owners/{{$owner->id}}" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1">{{ $owner->fullName() }}</h5>
-        <div class="d-flex flex-column align-items-end">
-            <p> {{ $owner->fullAddress() }}</p>
-            <p> {{ $owner->formattedPhoneNumber() }}</p>
-        </div>
-    </div>
-</a>
