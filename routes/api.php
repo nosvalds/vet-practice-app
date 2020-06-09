81871e01b3c76a83f0e1c909d9014fdeaa4c7cf1
Nik Osvalds <nosvalds@gmail.com>
2020-06-09 09:07:03 +0100

    remove auth:api route closure

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::group(["middleware" => ["auth:api"]], function () {
     // animals
